72fd4ec0eea6a986d3a0525b506dfe1bba64a0c0
codewilson64 <wilsongambit@gmail.com>
2023-11-30 20:58:44 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector("nav ul");
+
+hamburgerMenu.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
