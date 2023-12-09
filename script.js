764dce39569ee8c800a1137064ca27f26fd79249
codewilson64 <wilsongambit@gmail.com>
2023-12-09 10:57:41 +0700

    edit html css js file

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const nav = document.querySelector("nav ul");
+const emailButton = document.querySelector("#email-btn");
 
+// Toggle hamburger menu
 hamburgerMenu.addEventListener("click", function () {
   nav.classList.toggle("active");
+});
+
+// Prevent from scrolling up
+emailButton.addEventListener("click", function (e) {
+  e.preventDefault();
+});
+
+// Click anywhere to remove hamburger menu
+document.addEventListener("click", function (e) {
+  if (!hamburgerMenu.contains(e.target) && !nav.contains(e.target)) {
+    nav.classList.remove("active");
+  }
 });
